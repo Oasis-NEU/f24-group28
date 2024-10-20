0db68e1d73da57753b15f80506bbc8770819df83
elbert-p <elbert.p@northeastern.edu>
2024-10-20 13:30:09 -0400

    added table to workout

--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,14 +1,71 @@
+import React from 'react';
 import {
-    Table,
-    Thead,
-    Tbody,
-    Tfoot,
-    Tr,
-    Th,
-    Td,
-    TableCaption,
-    TableContainer,
-  } from '@chakra-ui/react'
-import data from '../data';
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Box,
+} from '@chakra-ui/react';
 
+function WorkoutTable({ dayData }) {
+  // Parse the dayData into a flat array for the table
+  const tableData = dayData.flatMap((dayExercises, dayIndex) => {
+    // Check if the day is a rest day
+    if (dayExercises.length === 1 && dayExercises[0].toLowerCase() === 'rest') {
+      return [{
+        day: dayIndex + 1,
+        exercise: 'Rest Day',
+        sets: '',
+        reps: '',
+        isFirstInDay: true,
+        isLastInDay: true,
+      }];
+    } else {
+      // For each exercise in the day
+      return dayExercises.map((exerciseData, idx) => {
+        const [exercise, sets, reps] = exerciseData;
+        return {
+          day: dayIndex + 1,
+          exercise: exercise,
+          sets: sets,
+          reps: reps,
+          isFirstInDay: idx === 0,
+          isLastInDay: idx === dayExercises.length - 1,
+        };
+      });
+    }
+  });
 
+  return (
+    <Box>
+      <Table variant="simple">
+        <Thead>
+          <Tr borderBottom="2px solid" borderColor="gray.400">
+            <Th>Day #</Th>
+            <Th>Exercise</Th>
+            <Th>Sets</Th>
+            <Th>Reps</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {tableData.map((row, index) => (
+            <Tr
+              key={index}
+              borderBottom={row.isLastInDay ? '2px solid' : '1px solid'}
+              borderColor={row.isLastInDay ? 'gray.200' : 'gray.200'}
+            >
+              <Td>{row.isFirstInDay ? row.day : ''}</Td>
+              <Td>{row.exercise}</Td>
+              <Td>{row.sets}</Td>
+              <Td>{row.reps}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </Box>
+  );
+}
+
+export default WorkoutTable;
--- src/pages/Workout.jsx
+++ src/pages/Workout.jsx
@@ -4,12 +4,30 @@ import Button from '../components/button';
 import WorkoutButton from '../components/workoutButton';
 import { CookiesProvider, useCookies } from 'react-cookie'
 import Schedule from '../components/schedule';
+import WorkoutTable from '../components/table';
+import data from '../data';
 
 function Workout() {
   const [cookies, setCookie] = useCookies(['user'])
+  let dayData = [];
+// console.log("dayData:", Object.keys(data[(cookies.input).toString()[0]][(cookies.input).toString()[1]]).length)
+  for(let i = 1; i<=Object.keys(data[(cookies.input).toString()[0]][(cookies.input).toString()[1]]).length; i++){
+    dayData.push(data[(cookies.input).toString()[0]][(cookies.input).toString()[1]][i])
+    // console.log(data[(cookies.input).toString()[0]][(cookies.input).toString()[1]][i])
+  }
+  console.log(dayData)
+  // const day1 = data[(cookies.input).toString()[0]][(cookies.input).toString()[1]][1]
+  // const day2 = data[(cookies.input).toString()[0]][(cookies.input).toString()[1]][2]
+  // const day3 = data[(cookies.input).toString()[0]][(cookies.input).toString()[1]][3]
+  // const day4 = data[(cookies.input).toString()[0]][(cookies.input).toString()[1]][4]
+  // const day5 = data[(cookies.input).toString()[0]][(cookies.input).toString()[1]][5]
+  // const day6 = data[(cookies.input).toString()[0]][(cookies.input).toString()[1]][6]
+  // const day7 = data[(cookies.input).toString()[0]][(cookies.input).toString()[1]][7]
   return (
-    <div className = "w-full">
-      <Schedule cookies = {cookies} ></Schedule>
+    //className = "w-full"
+    <div>
+      {/* <Schedule cookies = {cookies} ></Schedule> */}
+    <WorkoutTable dayData={dayData} />
     </div>
     
   );
