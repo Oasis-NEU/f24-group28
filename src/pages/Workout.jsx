721f35acfa4d15e25807c32462053c7ed318af8d
joshuachann <165356887+joshuachann@users.noreply.github.com>
2024-11-20 11:30:03 -0500

    mm

--- src/pages/Workout.jsx
+++ src/pages/Workout.jsx
@@ -3,10 +3,16 @@ import WorkoutTable from '../components/table';
 import { recommendedData, personalizedData, updatePersonalizedData} from '../data';
 
 function Workout() {
-  const mockData = {
-    "1": [["Push-Ups", "3", "15"], ["Pull-Ups", "3", "10"]],
-    "2": ["rest"],
-    "3": [["Squats", "4", "12"], ["Lunges", "4", "10"]],
+  const updateExercise = (day, originalExercise, newExercise) => {
+    if (personalizedData[day]) {
+      personalizedData[day] = personalizedData[day].map((exerciseData) => {
+        if (Array.isArray(exerciseData) && exerciseData[0] === originalExercise) {
+          return [newExercise, exerciseData[1], exerciseData[2]];
+        }
+        return exerciseData;
+      });
+      console.log(`Updated personalizedData for Day ${day}:`, personalizedData[day]);
+    }
   };
   
   // Log recommendedData to verify its structure
@@ -15,7 +21,7 @@ function Workout() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-800 text-white">
       <div className="w-full max-w-4xl flex justify-center">
-        <WorkoutTable dayData={personalizedData} />
+        <WorkoutTable dayData={personalizedData} updateExercise={updateExercise} />
       </div>
     </div>
   );
